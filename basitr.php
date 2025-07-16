b7acd5e4af5c56e3493650942a373ed84067c886
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-16 18:46:26 +0500

    Update and rename kamranr.php to basitr.php

--- basitr.php
+++ basitr.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Kamran';
+        $mail->Subject = 'Basit';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
