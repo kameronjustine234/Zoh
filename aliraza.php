0472892df23d3e506a8b4894bafb129ed0ad04d1
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-16 13:20:37 +0500

    Update and rename md.php to aliraza.php

--- aliraza.php
+++ aliraza.php
@@ -36,12 +36,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->setFrom('aliumair0044@gmail.com', 'Pk');
         $mail->addAddress('aliumair0044@gmail.com');
-        $mail->addAddress('adsmenigar@gmail.com');
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Muhammad';
+        $mail->Subject = 'Ali Raza';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
