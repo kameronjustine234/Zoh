86355b0132fb470d371ef63e26aa808465fd5365
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-16 18:02:36 +0500

    Update and rename aliraza.php to kamranr.php

--- kamranr.php
+++ kamranr.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Ali Raza';
+        $mail->Subject = 'Kamran';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
