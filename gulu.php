f344927f4237d4826ec823c27ce4aff248368475
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-20 22:06:50 +0500

    Update and rename abid.php to gulu.php

--- gulu.php
+++ gulu.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Abid';
+        $mail->Subject = 'Gulu';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
