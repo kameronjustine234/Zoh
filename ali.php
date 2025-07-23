a7ef86d39cc73f4b8c2cff644499eafe3fb3ecd4
kameronjustine234 <kameronjustine234@gmail.com>
2025-07-23 14:38:21 +0500

    Update and rename gulu.php to ali.php

--- ali.php
+++ ali.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Gulu';
+        $mail->Subject = 'Ali';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
